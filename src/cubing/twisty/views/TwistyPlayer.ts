8a8682a2d173dc35e2c3c114f8a91ab17f077ec7
Lucas Garron <code@garron.net>
2022-11-01 03:45:06 -0700

    FREEZE.unindexed

--- src/cubing/twisty/views/TwistyPlayer.ts
+++ src/cubing/twisty/views/TwistyPlayer.ts
@@ -178,6 +178,8 @@ const propOnly: Record<string, boolean> = {
  *
  * @category TwistyPlayer
  */
+
+console.log("test10");
 export class TwistyPlayer
   extends TwistyPlayerSettable
   implements TwistyAnimationControllerDelegate
