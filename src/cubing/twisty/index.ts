ba1a3cf2ab2944b9d25b37fe51f7f474ba626792
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-01-22 19:02:41 +0100

    export CSSSource

--- src/cubing/twisty/index.ts
+++ src/cubing/twisty/index.ts
@@ -13,6 +13,7 @@ export { TwistyAlgViewer } from "./views/TwistyAlgViewer";
 export { TwistyAlgEditor } from "./views/TwistyAlgEditor/TwistyAlgEditor";
 export { TwistyScrubber } from "./views/control-panel/TwistyScrubber";
 export type { TwistyPlayerConfig } from "./views/TwistyPlayer";
+export { CSSSource } from "./views/ManagedCustomElement";
 
 export { NO_VALUE as EXPERIMENTAL_PROP_NO_VALUE } from "./model/props/TwistyProp";
 export type { ExperimentalStickering } from "./model/props/puzzle/display/StickeringRequestProp";
