3616c0c1ebe2427a04ad697b3aeb3ba135476864
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-12-24 15:17:57 +0100

    add hover/active styling for leaves in TwistyAlgViewer

--- src/cubing/twisty/views/TwistyAlgViewer.css.ts
+++ src/cubing/twisty/views/TwistyAlgViewer.css.ts
@@ -10,22 +10,33 @@ export const twistyAlgViewerCSS = new CSSSource(
   display: inline;
 }
 
-a:not(:hover) {
+a {
   color: inherit;
   text-decoration: none;
-}
-
-twisty-alg-leaf-elem.twisty-alg-comment {
-  color: rgba(0, 0, 0, 0.4);
-}
-
-.wrapper.current-move {
-  background: rgba(66, 133, 244, 0.3);
   margin-left: -0.1em;
   margin-right: -0.1em;
   padding-left: 0.1em;
   padding-right: 0.1em;
   border-radius: 0.1em;
 }
+
+twisty-alg-leaf-elem.twisty-alg-comment {
+  color: rgba(0, 0, 0, 0.4);
+}
+
+.current-move a {
+  background: #7272CB;
+}
+
+@media(hover: hover) {
+  a:hover {
+    background: #7272CB;
+  }
+}
+
+a:active {
+  background: #565698;
+}
+
 `,
 );
